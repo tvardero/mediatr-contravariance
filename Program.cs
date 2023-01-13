5c47e1d36781cd7b91c4373f56d3e093e125d546
Rostyslav Morklianyk <rostislav.morklianyk@cargoclix.com>
2023-01-13 13:24:00 +0200

    fixup 1

--- Program.cs
+++ Program.cs
@@ -14,5 +14,11 @@ var teslaPainting = new PaintTeslaCommand { Color = "bright pink" };
 
 var mediator = serviceProvider.GetRequiredService<IMediator>();
 
-mediator.Publish(toyotaPainting);
-mediator.Publish(teslaPainting);
+mediator.Publish(toyotaPainting); // <-- unfortunately does nothing
+mediator.Publish(teslaPainting); // <-- unfortunately does nothing
+
+var h1 = serviceProvider.GetService<INotificationHandler<PaintCarCommandBase>>(); // <-- not null, OK
+var h2 = serviceProvider.GetService<INotificationHandler<PaintToyotaCommand>>(); // <-- Null
+var h3 = serviceProvider.GetService<INotificationHandler<PaintTeslaCommand>>(); // <-- Null
+
+Console.ReadLine();
